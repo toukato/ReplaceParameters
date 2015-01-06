33e2969ccdfb670c80a1928743f371d46b320cef
toukato <toukato0313@gmail.com>
2015-01-07 00:12:09 +0900

    修正対応
    値がからの場合、クエリストリングの場合の処理が漏れていたため修正。

--- ReplaceParameters.java
+++ ReplaceParameters.java
@@ -4,21 +4,27 @@ import java.util.regex.Pattern;
 
 public class ReplaceParameters {
 	
+	String regex1 = "=([a-zA-Z0-9%+]+)*&";
+	String regex2 = "=[a-zA-Z0-9%+]+$";
+	String regex3 = "=[a-zA-Z0-9%+]+(\\s*)";
+
+	Pattern p = Pattern.compile(regex1);
+	Pattern p1 = Pattern.compile(regex2);
+	Pattern p2 = Pattern.compile(regex3);
+	
 	public String replaceXSS(String requestBounds, int requestFirst, int requestEnd)
 	{
 		String requestReplace = requestBounds.substring(requestFirst, requestEnd);
 		StringBuilder sb = new StringBuilder(requestBounds);
 		
-		String regex1 = "=[a-zA-Z0-9%+]+&";
-		String regex2 = "=[a-zA-Z0-9%+]+$";
-		
-		Pattern p = Pattern.compile(regex1);
 		Matcher m = p.matcher(requestReplace);
 		String result = m.replaceAll("=\"</>xss&");
 
-		Pattern p1 = Pattern.compile(regex2);
 		Matcher m1 = p1.matcher(result);
 		result = m1.replaceAll("=\"</>xss");
+
+		Matcher m2 = p2.matcher(result);
+		result = m2.replaceAll("=\"</>xss ");
 
 		sb.replace(requestFirst, requestEnd, result);
 		
@@ -30,16 +36,14 @@ public class ReplaceParameters {
 		String requestReplace = requestBounds.substring(requestFirst, requestEnd);
 		StringBuilder sb = new StringBuilder(requestBounds);
 		
-		String regex1 = "=[a-zA-Z0-9%+]+&";
-		String regex2 = "=[a-zA-Z0-9%+]+$";
-		
-		Pattern p = Pattern.compile(regex1);
 		Matcher m = p.matcher(requestReplace);
-		String result = m.replaceAll("='\"<>xss/../../../../../../../../etc/passwd&");
+		String result = m.replaceAll("='\"<>xss../../../../../../../bin/sleep+60|&");
 
-		Pattern p1 = Pattern.compile(regex2);
 		Matcher m1 = p1.matcher(result);
-		result = m1.replaceAll("='\"<>xss/../../../../../../../../etc/passwd");
+		result = m1.replaceAll("='\"<>xss../../../../../../../bin/sleep+60|");
+
+		Matcher m2 = p2.matcher(result);
+		result = m2.replaceAll("=\"../../../../../../../bin/sleep+60| ");
 
 		sb.replace(requestFirst, requestEnd, result);
 		
@@ -51,16 +55,14 @@ public class ReplaceParameters {
 		String requestReplace = requestBounds.substring(requestFirst, requestEnd);
 		StringBuilder sb = new StringBuilder(requestBounds);
 		
-		String regex1 = "=[a-zA-Z0-9%+]+&";
-		String regex2 = "=[a-zA-Z0-9%+]+$";
-		
-		Pattern p = Pattern.compile(regex1);
 		Matcher m = p.matcher(requestReplace);
-		String result = m.replaceAll("='\"<>xss¥..¥..¥..¥..¥..¥..¥..¥..¥windows¥win.ini&");
+		String result = m.replaceAll("='\"<>xss..¥..¥..¥..¥..¥..¥..¥windows¥system32¥ping+–n+21+127.0.0.1|&");
 
-		Pattern p1 = Pattern.compile(regex2);
 		Matcher m1 = p1.matcher(result);
-		result = m1.replaceAll("='\"<>xss¥..¥..¥..¥..¥..¥..¥..¥..¥windows¥win.ini");
+		result = m1.replaceAll("='\"<>xss..¥..¥..¥..¥..¥..¥..¥windows¥system32¥ping+–n+21+127.0.0.1|");
+
+		Matcher m2 = p2.matcher(result);
+		result = m2.replaceAll("=\"</>xss..¥..¥..¥..¥..¥..¥..¥windows¥system32¥ping+–n+21+127.0.0.1| ");
 
 		sb.replace(requestFirst, requestEnd, result);
 		
